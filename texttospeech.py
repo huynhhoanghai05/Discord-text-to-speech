09b1a3e7790703fa74ae1fa2d41ee564b8d05b8d
root <root@HARIBLACK.localdomain>
2021-06-21 19:00:16 +0700

    Version 1.0

---
+++ texttospeech.py
@@ -0,0 +1,71 @@
+from gtts import gTTS
+
+import os
+import random
+import secrets
+import discord
+from discord.ext import commands
+
+client = discord.Client(activity=discord.Activity(name="Thay lời muốn nói <3"))
+language = 'vi'
+voice_client, channel = None, None
+
+@client.event
+async def on_ready():
+    print('Connected as')
+    print(client.user.name)
+    print('id: {}'.format(client.user.id))
+    print('------')
+ 
+
+@client.event
+async def on_message(message):
+
+    # display logs
+    print(message)
+    print(f"message: {message.content}")
+
+    # allow voice client to be used across different commands
+    global voice_client, channel
+
+    # check bot
+    if message.author == client.user:
+        return
+
+    # check command
+    split_msg = message.content.split()
+    if len(split_msg) >= 2 and split_msg[0] == 'tts':
+
+        # check voice channel
+        if voice_client:
+            if channel != voice_client.channel:
+                await voice_client.disconnect()
+
+        try:
+            channel = message.author.voice.channel
+        except AttributeError:
+            response = [
+            "Mày chưa vào kênh thoại mà dám gọi tao à? Ăn đấm không??",
+            "Vào kênh thoại đi mài!!",
+            "Bạn yêu vào kênh thoại đi rồi mình hú hí :3"
+            ]
+            await message.channel.send(random.choice(response))
+            print(f"response: {response}")
+
+        # create text to speech
+        tts = gTTS(text=" ".join(split_msg[1:]), lang=language, slow=False)
+        tts.save('tts.mp3')
+
+        voice_client = await channel.connect(reconnect=False)
+        audio_source = await discord.FFmpegOpusAudio.from_probe('tts.mp3')
+        voice_client.play(audio_source)
+
+        # disconnect voice when bot is finished speaking
+        while voice_client.is_playing():
+            continue
+    elif message.content == 'raise-exception':
+        raise discord.DiscordException
+
+
+client.run(secrets.TOKEN)
+
